b4061526411b6095e9123a98da0d3ef145f21260
l3nnartt <loesche.lennart@gmail.com>
2021-04-15 19:42:58 +0200

    1.1.1
    Upload of all Files

---
+++ build/sources/main/java/de/labymod/lennart/listener/TablistHeaderListener.java
@@ -0,0 +1,16 @@
+package de.labymod.lennart.listener;
+
+import de.labymod.lennart.Main;
+import net.labymod.api.events.TabListEvent;
+
+public class TablistHeaderListener implements TabListEvent {
+    @Override
+    public void onUpdate(Type type, String s, String s1) {
+        if (s.contains("Du spielst auf")) {
+            String[] servername = s.split("§6");
+            String serveroutput = servername[servername.length-1];
+            serveroutput = serveroutput.substring(0,serveroutput.length()-4);
+            Main.INSTANCE.latestserver = serveroutput;
+        }
+    }
+}
